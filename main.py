a1689c79f25aa06c927eff689f88d6545a48dd5f
William Lorey <wwlorey@gmail.com>
2019-02-22 12:23:56 -0600

    Add b* calculation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ai.heuristic import Heuristic
 from tj_wriggle.decoder import Decoder
 from util.args import Arguments
 from util.timer import Timer
+import numpy
 import os.path
 
 
@@ -21,7 +22,7 @@ DEFAULT_VALUES_STR = '  (1) Puzzle file path\n  (2) Solution file path'
 #    head/tail and the goal coordinate
 #  Heuristic.NUM_OBSTACLES:  Number of obstacles between the wriggler's head/tail 
 #    (whichever is closer to the goal) and the goal coordinate
-HEURISTIC = Heuristic.NUM_OBSTACLES
+HEURISTIC = Heuristic.MANHATTAN_DIST
 
 
 if __name__ == '__main__':
@@ -60,9 +61,43 @@ if __name__ == '__main__':
     if result.solution:
         # A solution has been found
         print('\nSolution found.')
-        print('N =', result.num_expanded_nodes)
-        print('d =', result.max_depth)
+            
+        # Calculate the effective branching factor b_star
+        # b_star^(d + 1) - (N + 1)*b_star + N = 0
+        N = result.num_expanded_nodes
+        d = result.max_depth
+        
+        # Construct the coefficient list in accordance with the above equation
+        coefficients = [N, -1 * N - 1]
 
+        # Set coefficients from b_star^2 to b_star^(d + 1) to 0
+        coefficients += [0] * d
+
+        # Reset coefficient of b_star^(d + 1) to 1
+        coefficients[-1] = 1
+
+        # Reverse the coefficient list to the order that numpy expects
+        coefficients = coefficients[::-1]
+        
+        # Generate the solutions
+        solution_list = numpy.roots(coefficients)
+
+        # Only take real valued solutions
+        solution_list = [numpy.real(solution) for solution in solution_list[numpy.isreal(solution_list)]]
+
+        # Find the valid solution
+        solution = None
+        for possible_solution in solution_list:
+            if possible_solution > 1:
+                solution = possible_solution
+                break
+        
+        if solution:
+            print('b* = %.5f' % solution)
+
+        else:
+            print('b* could not be found')
+        
         # Generate solution file
         puzzle.write_solution_file(soln_path, puzzle_path, result.solution.actions, result.solution.final_state, elapsed_time)
         
